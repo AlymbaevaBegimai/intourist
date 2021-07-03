12b0a961cb1d2674fc637f2e4f215e0f7dcdbf68
Begimai <sally.vatanabe.2002@gmail.com>
2021-07-03 19:49:14 +0600

    add core profile

--- core/models.py
+++ core/models.py
@@ -1,2 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import  User
+from django.db.models.fields.files import ImageField
 
+class Profile(models.Model):
+    user = models.OneToOneField(
+        to=User, on_delete=models.CASCADE, 
+        related_name='profile'
+    )
+    region = models.CharField(max_length=1, choices=(
+        ('B', 'Bishkek'),
+        ('C', 'Chuy'),
+        ('I', 'Issyk-Kul'),
+        ('N', 'Naryn'),
+        ('O', 'Osh'),
+        ('T', 'Talas'),
+        ('D', 'Djalal-Abad'),
+        ('A', 'Batken'),
+    ))
+
+    photo = models.ImageField(
+        upload_to='profile_photo ',
+        null=True, blank=True
+    )
+
+
+    def __str__(self):
+        return self.user.username
